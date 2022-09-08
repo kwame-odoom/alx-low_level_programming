8e5455170b49a9a15d8f6b94f0dbbfca58223670
John Amo <odoomkwame9@gmail.com>
2022-09-08 15:32:54 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#include<stdio.h>
-/***
- * main - Entry point
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  Return: 0
+*/
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
